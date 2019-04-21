8e3c3b0ced7a5656959026b95784df008520c7be
Renaud Fontana <renaud.fontana@enernoc.com>
2019-04-21 13:06:09 +0200

    added controls

--- src/components/GistsPage.vue
+++ src/components/GistsPage.vue
@@ -4,7 +4,21 @@
     <div class="with-data" v-if="viewer">
       <h1>Gists</h1>
       <span>Total: {{ viewer.gists.totalCount }}</span>
-      <div class="gist" v-for="(gist, index) in viewer.gists.nodes" :key="index">
+      <div class="control">
+        <span>Sort by:</span>
+        <select v-model="sort">
+          <option></option>
+          <option v-for="(sort, index) in sorts" :key="index" :value="sort">{{ sort }}</option>
+        </select>
+      </div>
+      <div class="control">
+        <span>Filter by:</span>
+        <select v-model="filter">
+          <option></option>
+          <option v-for="(extension, index) in extentions" :key="index" :value="extension">{{ extension }}</option>
+        </select>
+      </div>
+      <div class="gist" v-for="(gist, index) in gists" :key="index">
         <pre>{{ gist }}</pre>
       </div>
     </div>
@@ -18,7 +32,26 @@ import viewer from "../queries/gists";
 @Component({
   apollo: { viewer }
 })
-export default class GistsPage extends Vue {}
+export default class GistsPage extends Vue {
+  readonly sorts: string[] = ['createdAt', 'pushedAt', 'updatedAt'];
+  get extentions(): string[] {
+    const nested = (this.$data.viewer.gists.nodes as any[]).map((node) => (node.files as any[]).map((file) => file.extension));
+    const flattened = Array.prototype.concat.apply([], nested);
+    return Array.from(new Set<string>(flattened));
+  }
+  get gists(): any[] {
+    return this.$data.viewer.gists.nodes.filter(this.filterFn).sort(this.sortFn);
+  }
+  private filterFn(gist: any): boolean {
+    return (this.filter) ? gist.files.some((file: any) => file.extension === this.filter) : true;
+  }
+  private sortFn(gistA: any, gistB: any): number {
+    const [a, b] = [gistA, gistB].map((gist) => gist[this.sort]);
+    return (this.sort) ? (a > b) ? 1 : (a < b) ? -1 : 0 : 0;
+  }
+  sort = '';
+  filter = '';
+}
 </script>
 
 <style>
@@ -26,6 +59,15 @@ export default class GistsPage extends Vue {}
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.control {
+  display: flex;
+  justify-content: center;
+}
+.control > * {
+  margin: 0 5px;
+  width: 160px;
+  text-align: right;
 }
 .gist {
   max-width: 1024px;
